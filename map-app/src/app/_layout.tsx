56d6e083cf1d211d893ccb8b392b708e799b0307
AapoKiiskila <aapo.kiiskila@gmail.com>
2025-11-26 19:11:33 +0200

    Added React Native Paper

--- map-app/src/app/_layout.tsx
+++ map-app/src/app/_layout.tsx
@@ -1,16 +1,19 @@
+import { PaperProvider } from "react-native-paper"
 import React from "react"
 import { Stack } from "expo-router"
 
 export default function RootLayout() {
   return(
-    <Stack>
-      <Stack.Protected guard={true}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-        <Stack.Screen name="map/create-post" options={{ headerShown: true, title: "Create post", headerBackTitle: "Cancel" }}/>
-      </Stack.Protected>
-      <Stack.Protected guard={false}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }}/>
-      </Stack.Protected>
-    </Stack>
+    <PaperProvider>
+      <Stack>
+        <Stack.Protected guard={true}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+          <Stack.Screen name="map/create-post" options={{ headerShown: true, title: "Create post", headerBackTitle: "Cancel" }}/>
+        </Stack.Protected>
+        <Stack.Protected guard={false}>
+          <Stack.Screen name="(auth)" options={{ headerShown: false }}/>
+        </Stack.Protected>
+      </Stack>
+    </PaperProvider>
   )
 }
