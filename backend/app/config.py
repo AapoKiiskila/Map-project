1a8c1a2973b54e0883d78c9b8d0c1de0d9ffcdbb
Aapo Kiiskilä <aapo.kiiskila@gmail.com>
2025-12-28 12:26:16 +0200

    Modified config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,12 @@
+import pathlib
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+backend_directory = pathlib.Path(__file__).parent.parent.resolve()
+env_path = backend_directory / ".env"
 
 class Settings(BaseSettings):
   DATABASE_URL: str
 
-  model_config = SettingsConfigDict(env_file=".env")
+  model_config = SettingsConfigDict(env_file=env_path)
 
 settings = Settings()
