bfcb74b048598bbfda4e0b5ecd7b3a8df39f2997
AapoKiiskila <aapo.kiiskila@gmail.com>
2026-01-06 14:49:51 +0200

    Modified ProtectedLayout because back arrow was not showing on iOS

--- map-app/src/app/(protected)/_layout.tsx
+++ map-app/src/app/(protected)/_layout.tsx
@@ -7,10 +7,14 @@ export default function ProtectedLayout() {
     <UnreadContextProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-        <Stack.Screen name="(map)" options={{ headerShown: false }}/>
-        <Stack.Screen name="(sightings)" options={{ headerShown: false }}/>
-        <Stack.Screen name="(posts)" options={{ headerShown: false }}/>
-        <Stack.Screen name="(settings)" options={{ headerShown: false }}/>
+        <Stack.Screen name="(map)/create-post" options={{ headerShown: true, title: "Create post", headerBackTitle: "Cancel" }}/>
+        <Stack.Screen name="(map)/post/[post]/index" options={{ headerShown: true, title: "View post", headerBackTitle: "Back" }}/>
+        <Stack.Screen name="(map)/post/[post]/new-sighting" options={{ headerShown: true, title: "New sighting", headerBackTitle: "Cancel" }}/>
+        <Stack.Screen name="(sightings)/sighting/[sighting]/index" options={{ headerShown: true, headerBackTitle: "Back" }}/>
+        <Stack.Screen name="(posts)/post/[post]/index" options={{ headerShown: true, title: "View post", headerBackTitle: "Back" }}/>
+        <Stack.Screen name="(posts)/post/[post]/edit-post" options={{ headerShown: true, title: "Edit post", headerBackTitle: "Cancel" }}/>
+        <Stack.Screen name="(settings)/personal-information/index" options={{ headerShown: true, title: "Personal information", headerBackTitle: "Back", headerShadowVisible: false }}/>
+        <Stack.Screen name="(settings)/personal-information/change-username" options={{ headerShown: true, title: "Change username", headerBackTitle: "Cancel", headerShadowVisible: false }}/>
       </Stack>
     </UnreadContextProvider>
   )
