ab6c1073be6b6808c0a0f1ade496cc4e5d8dbf4c
AapoKiiskila <aapo.kiiskila@gmail.com>
2026-01-30 21:06:01 +0200

    Edited utils.py

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -2,5 +2,8 @@ import pwdlib
 
 password_hash = pwdlib.PasswordHash.recommended()
 
-def hash_password(password: str):
-  return password_hash.hash(password)
+def hash_password(plain_password: str):
+  return password_hash.hash(plain_password)
+
+def verify_password(plain_password: str, hashed_password: str):
+  return password_hash.verify(plain_password, hashed_password)
