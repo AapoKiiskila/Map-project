8ded315fa1f6d0bd9b201aca797325fe79f7249f
AapoKiiskila <aapo.kiiskila@gmail.com>
2026-02-05 12:57:08 +0200

    - Modified login screen

--- map-app/src/app/(root)/(auth)/login.tsx
+++ map-app/src/app/(root)/(auth)/login.tsx
@@ -3,18 +3,20 @@ import { ErrorResponse } from "../../../types/ErrorResponse"
 import { CustomButton } from "../../../components/CustomButton"
 import { CustomTextInput } from "../../../components/CustomTextInput"
 import { isStrongPassword } from "validator"
-import { Keyboard, KeyboardAvoidingView, Platform, Pressable, StyleSheet, View } from "react-native"
+import { Keyboard, Pressable, StyleSheet, Text, View } from "react-native"
 import { LoadingModal } from "../../../components/LoadingModal"
 import { matches } from "validator"
 import { TextInputInfoText } from "../../../components/TextInputInfoText"
 import { TokenResponse } from "../../../types/TokenResponse"
 import { UserInfoResponse } from "../../../types/UserInfoResponse"
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useState } from "react"
+import { useRouter } from "expo-router"
 import { UserContext } from "../../../context/UserContext"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { useHeaderHeight } from "@react-navigation/elements"
 
 export default function LoginScreen() {
-  const insets = useSafeAreaInsets()
+  const headerHeight = useHeaderHeight()
+  const router = useRouter()
   const {setUser, setToken} = useContext(UserContext)
   
   const URL = config.URL
@@ -26,61 +28,36 @@ export default function LoginScreen() {
   const [errorMessage, setErrorMessage] = useState<string>("")
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false)
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [behaviour, setBehaviour] = useState<"height" | undefined>("height")
-
-  useEffect(() => {
-    const showKeyboardListener = Keyboard.addListener("keyboardDidShow", () => {
-      setBehaviour("height")
-    })
-
-    const hideKeyboardListener = Keyboard.addListener("keyboardDidHide", () => {
-      setBehaviour(undefined)
-    })
-
-    return () => {
-      showKeyboardListener.remove()
-      hideKeyboardListener.remove()
-    }
-  }, [])
-
-  const checkUsername = (): void => {
-    if (!matches(username, "^[a-zA-Z0-9_\.\-]*$") || username.length <= 2) {
-      setUsernameError(true)
-    } else {
-      setUsernameError(false)
-    }
-  }
 
   const changeUsername = (text: string): void => {
-    if (!matches(text, "^[a-zA-Z0-9_\.\-]*$") || text.length <= 2) {
-      setUsernameError(true)
-    } else {
-      setUsernameError(false)
-    }
-
+    setUsernameError(false)
     setUsername(text)
   }
 
-  const checkPassword = (): void => {
-    if (!isStrongPassword(password, {minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 0})) {
-      setPasswordError(true)
-    } else {
-      setPasswordError(false)
-    }
-  }
-
   const changePassword = (text: string): void => {
-    if (!isStrongPassword(text, {minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 0})) {
-      setPasswordError(true)
-    } else {
-      setPasswordError(false)
-    }
-    
+    setPasswordError(false)
     setPassword(text)
   }
 
   const login = async (): Promise<void> => {
+    let credentialsError: boolean = false
     setIsSubmitted(true)
+
+    if (!matches(username, "^[a-zA-Z0-9_\.\-]*$") || username.length <= 2) {
+      credentialsError = true
+      setUsernameError(true)
+    }
+
+    if (!isStrongPassword(password, {minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 0})) {
+      credentialsError = true
+      setPasswordError(true)
+    }
+
+    if (credentialsError) {
+      setIsSubmitted(false)
+      return
+    }
+
     setIsLoading(true)
 
     const body = new URLSearchParams()
@@ -134,91 +111,98 @@ export default function LoginScreen() {
   }
   
   return(
-    <KeyboardAvoidingView 
-      style={styles.keyboardAvoidingView}
-      behavior={Platform.OS === "android" ? behaviour : "padding"}
-      keyboardVerticalOffset={Platform.OS === "android" ? 100 : 70}
-    >
-      <Pressable style={styles.mainContainer} onPress={Keyboard.dismiss}>
-        <View style={styles.upperContent}>
-          <View style={styles.usernameContainer}>
-            <CustomTextInput 
-              error={usernameError}
-              label="Username"
-              leftIcon="person"
-              maxLength={50}
-              onBlur={checkUsername}
-              onChangeText={changeUsername}
-              placeholder="Enter your username"
-              value={username}
-            />
-            <TextInputInfoText 
-              error={usernameError}
-              errorMessage={"Enter a valid username"}
-              style={{marginTop: 4}}
-              textLimit={50}
-              word={username}
-            />
-          </View>
-          <View style={styles.passwordContainer}>
-            <CustomTextInput 
-              error={passwordError}
-              label="Password"
-              leftIcon="lock-closed"
-              onBlur={checkPassword}
-              onChangeText={changePassword}
-              placeholder="Enter your password"
-              textType="password"
-              value={password}
-            />
-            <TextInputInfoText
-              error={passwordError}
-              errorMessage={"Enter a valid password"}
-              style={{marginTop: 4}}
-            />
-          </View>
-        </View>
-        <View style={[styles.lowerContent, {paddingBottom: insets.bottom}]}>
-          <CustomButton
-            disabled={!username || usernameError || !password || passwordError || isSubmitted}
-            label="Sign in"
-            onPress={login}
-          />
-        </View>
+    <Pressable style={[styles.mainContainer, {paddingTop: headerHeight / 3}]} onPress={Keyboard.dismiss}>
+      <View style={styles.usernameContainer}>
+        <CustomTextInput 
+          error={usernameError}
+          label="Username"
+          leftIcon="person"
+          maxLength={50}
+          onChangeText={changeUsername}
+          placeholder="Enter your username"
+          value={username}
+        />
+        <TextInputInfoText 
+          error={usernameError}
+          errorMessage={"Enter a valid username"}
+          style={{marginTop: 4}}
+          textLimit={50}
+          word={username}
+        />
+      </View>
+      <View style={styles.passwordContainer}>
+        <CustomTextInput 
+          error={passwordError}
+          label="Password"
+          leftIcon="lock-closed"
+          onChangeText={changePassword}
+          placeholder="Enter your password"
+          textType="password"
+          value={password}
+        />
+        <TextInputInfoText
+          error={passwordError}
+          errorMessage={"Enter a valid password"}
+          style={{marginTop: 4}}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          disabled={isSubmitted}
+          label="Sign in"
+          onPress={login}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text>Don't have an account? </Text>
+        <Pressable
+          onPress={() => router.replace("/register")}
+          style={({pressed}) => [
+            pressed && styles.pressablePressed,
+          ]}
+        >
+          <Text style={styles.signUpText}>Sign up</Text>
+        </Pressable>
+      </View>
 
+      {!usernameError && !passwordError &&
         <LoadingModal
           errorMessage={errorMessage} 
           isLoading={isLoading} 
           isVisible={isSubmitted} 
           onPress={hideError}
         />
-      </Pressable>
-    </KeyboardAvoidingView>
-    
+      }
+    </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
-  keyboardAvoidingView: {
-    backgroundColor: "rgba(255, 255, 255, 1)",
-    flex: 1,
-  },
   mainContainer: {
     alignItems: "center",
     backgroundColor: "rgba(255, 255, 255, 1)",
     flex: 1,
-    justifyContent: "space-between"
-  },
-  upperContent: {
-    width: "95%",
   },
   usernameContainer: {
-    marginTop: 10,
+    width: "75%",
   },
   passwordContainer: {
     marginTop: 10,
+    width: "75%",
   },
-  lowerContent: {
-    width: "95%",
+  buttonContainer: {
+    width: "75%",
+    marginTop: 20,
+  },
+  textContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  pressablePressed: {
+    opacity: 0.5,
+  },
+  signUpText: {
+    color: "rgb(0, 0, 255)",
+    fontWeight: 500,
   },
 })
