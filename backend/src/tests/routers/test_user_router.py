c0dcc65384f9ba1d86296039861ecabb68a8d451
Aapo Kiiskilä <aapo.kiiskila@gmail.com>
2026-03-18 15:38:54 +0200

    Created a fixture for adding data to the test database and modified tests

--- backend/src/tests/routers/test_user_router.py
+++ backend/src/tests/routers/test_user_router.py
@@ -1,5 +1,3 @@
-import src.models
-
 def test_create_new_user(client):
   response = client.post(
     url="/users",
@@ -13,16 +11,7 @@ def test_create_new_user(client):
   assert response.status_code == 201
   assert response.json() == {"message": "A new account has been successfully created"}
 
-def test_get_user_info(client, db):
-  user = src.models.User(
-    username = "JohnDoe",
-    email = "john.doe@gmail.com",
-    hashed_password = "cgtrenuotjko74m27g04786snmvtso5"
-  )
-
-  db.add(user)
-  db.commit()
-
+def test_get_user_info(client, insert_database_data):
   response = client.get(url="/users")
 
   assert response.status_code == 200
@@ -32,46 +21,19 @@ def test_get_user_info(client, db):
     "email": "john.doe@gmail.com"
   }
 
-def test_change_new_username(client, db):
-  user = src.models.User(
-    username = "JohnDoe",
-    email = "test.user@gmail.com",
-    hashed_password = "cgtrenuotjko74m27g04786snmvtso5"
-  )
-
-  db.add(user)
-  db.commit()
-
+def test_change_new_username(client, insert_database_data):
   response = client.put(url="/users/update-username", json={"username": "JohnDoe123"})
 
   assert response.status_code == 200
   assert response.json() == {"message": "Username has been changed", "username": "JohnDoe123"}
 
-def test_change_new_email(client, db):
-  user = src.models.User(
-    username = "JohnDoe",
-    email = "john.doe@gmail.com",
-    hashed_password = "cgtrenuotjko74m27g04786snmvtso5"
-  )
-
-  db.add(user)
-  db.commit()
-
+def test_change_new_email(client, insert_database_data):
   response = client.put(url="/users/update-email", json={"email": "john.doe123@gmail.com"})
 
   assert response.status_code == 200
   assert response.json() == {"message": "Email address has been changed", "email": "john.doe123@gmail.com"}
 
-def test_change_new_password(client, db):
-  user = src.models.User(
-    username = "JohnDoe",
-    email = "john.doe@gmail.com",
-    hashed_password = "cgtrenuotjko74m27g04786snmvtso5"
-  )
-
-  db.add(user)
-  db.commit()
-
+def test_change_new_password(client, insert_database_data):
   response = client.put(url="/users/update-password", json={"password": "Secret456"})
 
   assert response.status_code == 200
