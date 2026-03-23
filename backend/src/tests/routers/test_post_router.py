090086742e88bbd62b17cfde74c702a7139fa172
Aapo Kiiskilä <aapo.kiiskila@gmail.com>
2026-03-23 20:02:33 +0200

    Created two new tests

--- backend/src/tests/routers/test_post_router.py
+++ backend/src/tests/routers/test_post_router.py
@@ -19,3 +19,36 @@ def test_fetch_all_posts(client, insert_database_data):
   assert 3 not in every_fetched_post_id
   assert 4 in every_fetched_post_id
   assert 5 in every_fetched_post_id
+
+def test_fetch_user_posts(client, insert_database_data):
+  response = client.get(url="/posts/my-posts")
+
+  every_fetched_post_id = []
+
+  for post in response.json():
+    every_fetched_post_id.append(post["id"])
+
+  assert response.status_code == 200
+  assert len(response.json()) == 2
+  assert 1 in every_fetched_post_id
+  assert 2 in every_fetched_post_id
+  assert 3 not in every_fetched_post_id
+  assert 4 not in every_fetched_post_id
+  assert 5 not in every_fetched_post_id
+
+def test_create_new_post(client, insert_database_data):
+  response = client.post(
+    url="/posts/create-post",
+    json={
+      "title": "Test",
+      "details": "Test test test test test test.",
+      "type": "item",
+      "latitude": 65.67871878251934,
+      "longitude": 25.487784029247916,
+      "user_id": 1
+    }
+  )
+
+  assert response.status_code == 201
+  assert response.json() == {"message": "Post created successfully"}
+  
